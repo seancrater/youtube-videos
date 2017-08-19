5c11e020a7da9b9180cf0dd9ba19ef5c8a5f6d4d
seancrater <sean@seancrater.com>
2017-08-19 19:19:55 -0400

    Refactoring and adding SCSS baseline files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var express = require('express');
-var config = require('./webpack.config');
+var config = require('../webpack.config');
 
 var app = express();
 var compiler = webpack(config);
@@ -13,7 +13,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(3000, function(err) {
