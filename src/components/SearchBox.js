3304aeb898b34afffc28a88a53e10da8501fa9b2
Sean Crater <sean@seancrater.com>
2017-11-26 18:24:08 -0500

    Starting SearchBox component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 export default class SearchBox extends React.Component {
+  handleSubmit() {
+    const { value } = this.searchInput;
+    console.log(value);
+  }
+
   render() {
     return (
-      <input type='text' />
+      <div className='SearchBox'>
+        <input
+          className='SearchBox__input'
+          placeholder='Search Surf Videos'
+          ref={ this => this.searchInput = this }
+          type='text'
+        />
+        <button
+          children='Search'
+          className='SearchBox__button'
+          onClick={ this.handleSubmit() }
+        />
+      </div>
     );
   }
 }
