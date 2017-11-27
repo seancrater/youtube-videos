a7e16571f78f7c4a27facdb45902b183d0ef48b1
Sean Crater <sean@seancrater.com>
2017-11-27 08:37:40 -0500

    Fixing link

--- src/components/VideoPreview.js
+++ src/components/VideoPreview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class VideoPreview extends React.Component {
   render() {
@@ -7,14 +8,14 @@ export default class VideoPreview extends React.Component {
 
     return (
       <div className='VideoPreview'>
-        <a href={`/video/${ id.videoId }`}>
+        <Link to={`/video/${ id.videoId }`}>
           <img
             alt={`${ snippet.title } | ${ snippet.channelTitle }`}
             className='VideoPreview__image'
             src={ snippet.thumbnails.medium.url }
           />
           <h3 className='VideoPreview__title'>{ snippet.title }</h3>
-        </a>
+        </Link>
         <p className='VideoPreview__description'>{ snippet.description }</p>
         <p className='VideoPreview__channel'>{ snippet.channelTitle }</p>
       </div>
